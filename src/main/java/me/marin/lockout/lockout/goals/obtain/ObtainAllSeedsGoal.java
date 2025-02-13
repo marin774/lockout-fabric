3d3f239053c3d9e4e9f140421f9188f156dd200d
marin774 <marinbanozic@gmail.com>
2025-02-14 00:46:24 +0100

    Change 'Obtain all seeds' to 'Obtain 4 unique types of seeds' (include torchflower seeds)

--- src/main/java/me/marin/lockout/lockout/goals/obtain/ObtainAllSeedsGoal.java
+++ src/main/java/me/marin/lockout/lockout/goals/obtain/ObtainAllSeedsGoal.java
@@ -1,14 +1,14 @@
 package me.marin.lockout.lockout.goals.obtain;
 
-import me.marin.lockout.lockout.interfaces.ObtainAllItemsGoal;
+import me.marin.lockout.lockout.interfaces.ObtainSomeOfTheItemsGoal;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 
 import java.util.List;
 
-public class ObtainAllSeedsGoal extends ObtainAllItemsGoal {
+public class ObtainAllSeedsGoal extends ObtainSomeOfTheItemsGoal {
 
-    private static final List<Item> ITEMS = List.of(Items.WHEAT_SEEDS, Items.BEETROOT_SEEDS, Items.MELON_SEEDS, Items.PUMPKIN_SEEDS);
+    private static final List<Item> ITEMS = List.of(Items.WHEAT_SEEDS, Items.BEETROOT_SEEDS, Items.MELON_SEEDS, Items.PUMPKIN_SEEDS, Items.TORCHFLOWER_SEEDS);
 
     public ObtainAllSeedsGoal(String id, String data) {
         super(id, data);
@@ -21,7 +21,11 @@ public class ObtainAllSeedsGoal extends ObtainAllItemsGoal {
 
     @Override
     public String getGoalName() {
-        return "Obtain every type of Seed";
+        return "Obtain 4 unique types of Seeds";
     }
 
+    @Override
+    public int getAmount() {
+        return 4;
+    }
 }
